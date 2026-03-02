97d31f21eb64363f0e8c5542cf02524a95bf685e
Filippo Ziche <filippo.ziche@gmail.com>
2026-03-02 09:49:34 +0100

    feat: add example function

--- usage.cpp
+++ usage.cpp
@@ -4,21 +4,43 @@
 
 #include <cstdio>
 
+void calculate_z_slice(
+    float* pmap,        // The potential map slice (width x height) 
+    const int width,    // Width of the slice
+    const int height,   // Height of the slice
+    const int z,        // What z slice we are calculating
+    const Particle* ps, // Particles of the system
+    const int n)        // Number of particles
+{ 
+}
+
 int main() {
     
+    const int W = 64;
+    const int H = 64;
+    const int D = 64;
+
     // Create 10.000 particles in a 64x64x64 space
-    const Particle* particles = epm_create_particles(10000, 64, 64, 64);
+    const Particle* particles = epm_create_particles(10000, W, H, D);
     
     // Create two potential maps slices of size 64x64 initialized to zero
-    float* pmapA = epm_create_pmap_zeroed(64, 64);
-    float* pmapB = epm_create_pmap_zeroed(64, 64);
-    pmapB[100] = 0.0004f;
+    float* pmapA = epm_create_pmap_zeroed(W, H);
+    float* pmapB = epm_create_pmap_zeroed(W, H);
+    
+    // Calculate the potential map slice at z=32
+    const int Z = 32;
+    calculate_z_slice(pmapA, W, H, Z, particles, 10000);
 
     // Check if the potential maps are approximately equal
-    if (epm_check_pmap_slices(pmapA, pmapB, 64, 64)) {
-        printf("Potential maps are approximately equal.     \n");
+    if (epm_check_pmap_slices(pmapA, pmapB, W, H)) {
+        printf("Potential maps are approximately equal.\n");
     } else {
-        printf("Potential maps are NOT approximately equal. \n");
+        printf("Potential maps are NOT approximately equal.\n");
+        for (int i = 0; i < W * H; i++) {
+            if (pmapA[i] != pmapB[i]) {
+                printf("Difference at index %d: pmapA=%f, pmapB=%f\n", i, pmapA[i], pmapB[i]);
+            }
+        }
     }
 
     delete[] particles;
